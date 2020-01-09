8b861c496374f42a9be891fb379d50fb2b844d71
Ants <code.herder@recombulator.net>
2020-01-08 23:47:57 -0500

    Add view model MainWindowViewModel

---
+++ WpfTiles/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace WpfTiles
+{
+    class MainWindowViewModel
+    {
+        public ObservableCollection<ColorInfo> Colors { get; }
+
+        public MainWindowViewModel()
+        {
+            Colors = new ObservableCollection<ColorInfo>(ColorInfo.EnumerateAllColors().OrderBy(c => c.Name));
+        }
+    }
+}
